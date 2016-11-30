1e8e556d305977c5c01735d3d0df1bac3f73ab8e
Tejas Rawal <trawal90@gmail.com>
2016-11-30 01:59:14 -0600

    Fetch list of places through Google places API for defined map area

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,13 +1,51 @@
 import React from 'react'
 import Map, {GoogleApiWrapper} from 'google-maps-react'
 
+import {searchNearby} from 'utils/googleApiHelpers'
+
 export class Container extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      places: [],
+      pagination: null
+    }
+  }
+
+  onReady(mapProps, map) {
+    const google = this.props.google
+    const opts = {
+      location: map.center,
+      radius: '500',
+      types: ['cafe']
+    }
+
+    searchNearby(google, map, opts)
+      .then((results, pagination) => {
+        this.setState({
+          places: results,
+          pagination
+        })
+      })
+      .catch((status, result) => {
+        console.log('Error occured', status)
+      })
+  }
+
   render() {
     return (
       <div>
         Hello from the container
         <Map
-          google={this.props.google} />
+          onReady={this.onReady.bind(this)}
+          google={this.props.google}
+          visible={false}>
+
+          {this.state.places.map(place => {
+            return (<div key={place.id}>{place.name}</div>)
+          })}
+        </Map>
       </div>
     )
   }
