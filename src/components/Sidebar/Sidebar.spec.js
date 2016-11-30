2cba8a253928a3263c17d938f0f08c56b15b2164
Tejas Rawal <trawal90@gmail.com>
2016-11-30 16:44:58 -0600

    Add spec for Sidebar component

--- src/components/Sidebar/Sidebar.spec.js
+++ src/components/Sidebar/Sidebar.spec.js
@@ -3,6 +3,7 @@ import { expect } from 'chai'
 import { shallow } from 'enzyme'
 
 import Sidebar from './Sidebar'
+import styles from './styles.module.css'
 
 describe('<Sidebar />', () => {
   let sidebar
@@ -11,5 +12,7 @@ describe('<Sidebar />', () => {
   })
 
   it('contains div with sidebar class', () => {
+    expect(sidebar.find(`.${styles.sidebar}`))
+      .to.be.defined;
   })
 })
