3ec72c89a16b13a5da445edd3724179592a6d08c
Tejas Rawal <trawal90@gmail.com>
2016-12-07 17:05:08 -0600

    Create map component that will hold the map of the area

---
+++ src/views/Main/Map/Map.js
@@ -0,0 +1,15 @@
+import React, {PropTypes as T} from 'react'
+import classnames from 'classnames'
+
+import Map from 'google-maps-react'
+import styles from './styles.module.css'
+
+export class MapComponent extends React.Component {
+  render() {
+    <Map google={this.props.google}
+         className={styles.map}>
+    </Map>
+  }
+}
+
+export default MapComponent
